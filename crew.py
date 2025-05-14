11a4518012f15f99fd6a3a3ae3e23178f45f8983
lalitjoshi1989 <ljoshi1989@gmail.com>
2025-05-15 01:18:50 +0530

    adding files

---
+++ crew.py
@@ -0,0 +1,18 @@
+from crewai import Crew, Process, LLM
+from agents import blog_researcher, blog_writer
+from tasks import research_task, write_task
+
+crew = Crew(
+    agents = [blog_researcher, blog_writer],
+    tasks = [research_task, write_task],
+    proces = Process.sequential,
+    memory = True,
+    cache = True,
+    max_rpm = 100,
+    share_crew = True
+)
+
+
+#start task execution process with enhanced feedback
+result = crew.kickoff(inputs = {'topic':'AI VS ML VS DL vs Data Science'})
+print(result)
